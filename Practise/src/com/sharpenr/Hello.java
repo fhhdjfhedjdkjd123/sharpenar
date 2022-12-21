7c6b0ec6333a910f57143bb55cc1ee18e6e5f624
HP <HP@DESKTOP-QJ1S2UQ>
2022-12-21 11:50:19 +0530

    first commit uploading files

---
+++ Practise/src/com/sharpenr/Hello.java
@@ -0,0 +1,31 @@
+package com.sharpenr;
+public class Hello {
+//Factorial........
+//	public static int factorialOfNum(int n) {
+//		if(n>=1) {
+//			return n * factorialOfNum(n-1) ;
+//		}
+//		else {
+//			return 1;
+//		}
+//	}
+//	public static void main(String[] args) {
+//		int n = 5;
+//		long fac = factorialOfNum(n);
+//		System.out.println(fac);
+//	}
+	
+//Fibonacci.......
+//    public int fib(int n){
+//
+//		if(n <= 1) {
+//
+//			return n;
+//
+//		} else {
+//			return fib(n-1) + fib(n-2);
+//		}
+//	}
+//Permutations.......
+	
+}
